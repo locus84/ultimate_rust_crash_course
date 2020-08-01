e7d037c7393a418183da32fbd97a55a8ae7624e2
locus84 <locus84@gmail.com>
2020-08-02 04:42:15 +0900

    solving excercises

--- exercise/d-control-flow-strings/src/main.rs
+++ exercise/d-control-flow-strings/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     // ...produces the equivalent of
     //
     //     vec!["apple".to_string(), "banana.to_string()"]
-    let args: Vec<String> = std::env::args().skip(1).collect();
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
 
     // This consumes the `args` vector to iterate through each String
     for arg in args {
@@ -19,6 +19,16 @@ fn main() {
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
+        if arg == "sum" {
+            sum();
+        }
+        else if arg == "double" {
+            double();
+        }
+        else
+        {
+            count(arg);
+        }
 
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
@@ -32,6 +42,9 @@ fn sum() {
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
 
+    for to_add in 7..=23 {
+        sum += to_add;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -43,6 +56,11 @@ fn double() {
     // by 2) before it is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
+    while x <= 500 {
+        x *= 2;
+        count += 1;
+    }
+
 
     println!("You can double x {} times before it is larger than 500", count);
 }
@@ -52,7 +70,12 @@ fn count(arg: String) {
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut print_count = 0_isize;
+    loop {
+        if print_count >= 8 { break; }
+        print!("{} ", arg);
+        print_count += 1_isize;
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
